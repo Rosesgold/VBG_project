ad3d2176cc23a82a7bf815952dee1762a0116bc6
Rosesgold <aaronmercy@gmail.com>
2023-10-13 00:36:51 +0300

    commit #5

--- Shablya/Shablya/urls.py
+++ Shablya/Shablya/urls.py
@@ -4,6 +4,6 @@ from MainApp.views import index_page, cabinet_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_page),
+    path('', index_page, name='mainpage'),
     path('cabinet/', cabinet_page, name='cabinet')
 ]

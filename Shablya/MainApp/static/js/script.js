3293c5a76c9814155d3089183ffe2f405dda0d23
Rosesgold <aaronmercy@gmail.com>
2023-10-12 11:17:41 +0300

    commit #3

--- Shablya/MainApp/static/js/script.js
+++ Shablya/MainApp/static/js/script.js
@@ -1,7 +1,16 @@
 function toggleLike(button) {
     var likeIcon = button.querySelector('.like-icon');
     var likedIcon = button.querySelector('.liked-icon');
+
+    // Переключаємо класи, щоб приховати одне зображення і показати інше
     likeIcon.classList.toggle('hidden');
     likedIcon.classList.toggle('hidden');
+}
+
+function toggleSave(button) {
+    var saveIcon = button.querySelector('.save-icon');
+    var savedIcon = button.querySelector('.saved-icon');
+    saveIcon.classList.toggle('hidden');
+    savedIcon.classList.toggle('hidden');
 }
 
